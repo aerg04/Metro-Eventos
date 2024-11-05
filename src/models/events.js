de1877fa8150c0ff151e0a2473af425fc736e75f
Andres Rangel <andres.rangel@correo.unimet.edu.ve>
2024-11-05 07:21:22 -0400

    conexion con eventos

--- src/models/events.js
+++ src/models/events.js
@@ -1,5 +1,41 @@
- import events from "../assets/events.js"
+import events from "../assets/events.js"
+import axiosInstance from "../axiosConfig.js";
 
-export function getEvents(){
-    return events;
+// Function to fetch all events from the endpoint
+export async function getEvents() {
+    try {
+        const response = await axiosInstance.get('/events');
+        const fetchedEvents = response.data;
+        return fetchedEvents;
+    } catch (error) {
+        console.error("Failed to fetch events:", error);
+    }
+}
+
+// Function to convert an image file to a base64 string
+function convertImageToBase64(file) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onloadend = () => {
+            resolve(reader.result);
+        };
+        reader.onerror = reject;
+        reader.readAsDataURL(file);
+    });
+}
+
+// Function to add a new event
+export async function addEvent(newEvent) {
+    try {
+        if (newEvent.path) {
+            const base64Image = await convertImageToBase64(newEvent.path);
+            newEvent.path = base64Image;
+        }
+        // Send the new event data to the endpoint
+        const response = await axiosInstance.post('/events', newEvent);
+        const savedEvent = response.data;
+        return savedEvent;
+    } catch (error) {
+        console.error("Failed to add event:", error);
+    }
 }

e1a05ee2b7ea6b78f02eaeb14a1bab578d16ea0c
bryan <bryanfredecari@gmail.com>
2024-11-25 18:02:19 -0400

    Calendario con eventos incorporado en la sección de mis eventos

--- src/pages/MyEvents.jsx
+++ src/pages/MyEvents.jsx
@@ -1,7 +1,18 @@
 import EventCom from "../components/EventCom";
 import { Link } from 'react-router-dom';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import { useState } from 'react';
 
 export default function MyEvents({onClick,loading ,events}) {
+    const [selectedDate, setSelectedDate] = useState(new Date());
+
+    const eventDates = events.map(event => new Date(event.date));
+
+    const handleDateChange = (date) => {
+        setSelectedDate(date);
+    };
+
     return(
     <>
     <div className="pt-4 px-10 gap-y-2">
@@ -12,6 +23,33 @@ export default function MyEvents({onClick,loading ,events}) {
     </div>
     <h1 className="font-bold text-2xl">Mis eventos</h1>
     </div>
+
+            <div className="min-h-screen p-2 grid lg:grid-cols-[min-content,1fr] gap-4">
+
+                <div className="bg-white shadow-md rounded-md p-4">
+                    <h2 className="font-bold text-lg mb-4">Calendario de eventos</h2>
+                    <Calendar
+                        onChange={handleDateChange}
+                        value={selectedDate}
+                            tileClassName={({ date, view }) => {
+
+                            const defaultClass = 'bg-gray-200 text-gray-600';
+
+                            const isEventDate = eventDates.some(
+                                (d) => d.toISOString().split("T")[0] === date.toISOString().split("T")[0]
+                            );
+
+                            const isWeekend = [0, 6].includes(date.getDay());
+
+                            if (isEventDate) return "react-calendar__tile--hasEvent";
+                            if (isWeekend) return "react-calendar__tile--weekend";
+
+              return "defaultClass";
+            }}
+            className="compact-calendar"
+                    />
+                </div>
+
     <div className="min-h-screen p-2 grid justify-evenly grid-cols-[repeat(auto-fill,minmax(256px,1fr))] lg:grid-cols-4">
             {loading ? (
                     <div className="main-container">
@@ -39,8 +77,9 @@ export default function MyEvents({onClick,loading ,events}) {
                   <p className="text-2xl font-semibold text-center mt-12 mb-120">No hay eventos</p>
                   </div>
                   </div>
-            )} 
-    </div>
-    </>
-    )
+                    )}
+                </div>
+            </div>
+        </>
+    );
 }
